c479eeccc375224e8da13ae5f23e14252584cb3e
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-07-17 21:35:14 +0300

    Arranged the interface elements

--- createAnyColorInView/ViewController.swift
+++ createAnyColorInView/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorChangeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorChangeView.layer.cornerRadius = 15
     }
 
 
